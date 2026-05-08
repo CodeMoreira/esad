c7c5f60e53aafbc6efb7c91e606e64a0bcd7ff9f
CodeMoreira <gabrielmoreirailustra@gmail.com>
2026-05-08 15:55:27 -0300

    fix: rspack configs

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -2,10 +2,11 @@ const path = require('node:path');
 const fs = require('node:fs');
 const Repack = require('@callstack/repack');
 const { ExpoModulesPlugin } = require('@callstack/repack-plugin-expo-modules');
+const rspack = require('@rspack/core');
+const dotenv = require('dotenv');
 
 /**
- * ESAD Re.Pack Plugin Wrapper (v2.0 - POC Mirror)
- * Totalmente alinhado com a POC funcional para Expo 52 + Re.Pack 5.
+ * ESAD Re.Pack Plugin Wrapper
  * 
  * @param {Object} env Rspack environment
  * @param {Object} options 
@@ -14,9 +15,26 @@ function withESAD(env, options) {
   const { platform, dev } = env;
   const isDev = dev !== false;
 
+  const dirname = options.dirname;
+
   process.env.EXPO_OS = platform;
 
-  const dirname = options.dirname;
+  // --- Automatic Environment Variable Logic ---
+  // Load .env file from the user's project root
+  const envPath = path.resolve(dirname, '.env');
+  const envVars = dotenv.config({ path: envPath }).parsed || {};
+
+  const publicEnvs = {};
+  // Map variables from .env file and system variables that start with EXPO_PUBLIC_
+  const allSources = { ...process.env, ...envVars };
+
+  Object.keys(allSources).forEach((key) => {
+    if (key.startsWith('EXPO_PUBLIC_')) {
+      publicEnvs[`process.env.${key}`] = JSON.stringify(allSources[key] || '');
+    }
+  });
+  // --------------------------------
+
   const pkgPath = path.resolve(dirname, 'package.json');
   const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
   const id = options.id.replace(/-/g, '_');
@@ -52,6 +70,7 @@ function withESAD(env, options) {
                   require.resolve('babel-plugin-transform-define'),
                   {
                     'process.env.EXPO_OS': platform,
+                    ...publicEnvs,
                   },
                 ],
               ],
@@ -63,6 +82,7 @@ function withESAD(env, options) {
     },
     plugins: [
       new Repack.RepackPlugin(),
+      new rspack.DefinePlugin(publicEnvs),
       new Repack.plugins.ModuleFederationPluginV2({
         name: id,
         filename: `${id}.container.js.bundle`,
